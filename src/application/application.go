9ac2845a7380738ef15b1e93063528e5d7eb5b20
Dragan Ćulibrk <draganculibrk9@uns.ac.rs>
2021-07-08 11:38:54 +0200

    #16 Add Prometheus middleware

--- src/application/application.go
+++ src/application/application.go
@@ -9,17 +9,34 @@ import (
 	"github.com/Nistagram-Organization/nistagram-shared/src/utils/prometheus_handler"
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 var (
-	router = gin.Default()
+	router        = gin.Default()
+	requestsCount = prometheus_handler.GetHttpRequestsCounter()
+	requestsSize  = prometheus_handler.GetHttpRequestsSize()
+	uniqueUsers   = prometheus_handler.GetUniqueClients()
 )
 
-func StartApplication() {
+func configureCORS() {
 	corsConfig := cors.DefaultConfig()
 	corsConfig.AllowAllOrigins = true
 	corsConfig.AddAllowHeaders("Authorization")
 	router.Use(cors.New(corsConfig))
+}
+
+func registerPrometheusMiddleware() {
+	prometheus.Register(requestsCount)
+	prometheus.Register(requestsSize)
+	prometheus.Register(uniqueUsers)
+
+	router.Use(prometheus_handler.PrometheusMiddleware(requestsCount, requestsSize, uniqueUsers))
+}
+
+func StartApplication() {
+	configureCORS()
+	registerPrometheusMiddleware()
 
 	authGrpcClient := auth_grpc_client.NewAuthGrpcClient()
 	postGrpcClient := post_grpc_client.NewPostGrpcClient()
